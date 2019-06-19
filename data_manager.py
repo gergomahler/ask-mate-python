e6314e52dba5579ecb1951285189fb587d621161
gergomahler <mahlergergo@gmail.com>
2019-06-19 11:32:29 +0200

    question list and add-question fix

--- data_manager.py
+++ data_manager.py
@@ -1,12 +1,30 @@
-import time
+from datetime import datetime
 import connection
 
 question_file = 'sample_data/question.csv'
 answer_file = 'sample_data/answer.csv'
 
 
+def change_unix_to_utc(questions):
+    for question in questions:
+        timestamp = int(question["submission_time"])
+        question["submission_time"] = \
+            datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+    return questions
+
+
+def sorting_by_submission_time(questions):
+
+    return sorted(questions, key=lambda k: k['submission_time'], reverse=True)
+
+
 def get_questions():
-    return connection.read_from_file(question_file)
+    questions = connection.read_from_file(question_file)
+    sorted_questions = sorting_by_submission_time(questions)
+
+    return change_unix_to_utc(sorted_questions)
+
 
 
 """

9b1718293dcd9d41ce1bfe191e60d81f58867af1
Attila Boros <boros.attila17@gmail.com>
2019-07-01 11:58:16 +0200

    removed unnecessary else/elif statements

--- server.py
+++ server.py
@@ -17,9 +17,9 @@ def main_page():
 def add_question():
     if request.method == 'GET':
         return render_template('add-question.html')
-    elif request.method == 'POST':
-        question_id = data_manager.add_new_question(request.form)
-        return redirect(f'/question/{question_id}')
+
+    question_id = data_manager.add_new_question(request.form)
+    return redirect(f'/question/{question_id}')
 
 
 @app.route('/question/<question_id>')
@@ -33,10 +33,10 @@ def show_question_details(question_id):
 def add_answer(question_id):
     if request.method == 'GET':
         return render_template('new-answer.html', question_id=question_id)
-    else:
-        data_manager.add_new_answer(request.form, question_id)
 
-        return redirect(f'/question/{question_id}')
+    data_manager.add_new_answer(request.form, question_id)
+
+    return redirect(f'/question/{question_id}')
 
 
 if __name__ == '__main__':
